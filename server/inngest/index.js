fcf9e0c028da0ec5c16ffbd9a63097db52690b8a
sandeep-kumar-21 <sandeepkumarnitrr@gmail.com>
2025-06-18 01:18:39 +0530

    error fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -26,14 +26,8 @@ const syncUserDeletion = inngest.createFunction(
     {id: 'delete-user-width-clerk'},
     {event: 'clerk/user.deleted'},
     async({event}) => {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
-        }
-        await User.findByIdAndDelete(id,userData)
+        const {id} = event.data
+        await User.findByIdAndDelete(id)
     }
 )
 
@@ -42,9 +36,15 @@ const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
     {event: 'clerk/user.updated'},
     async({event}) => {
-        const {id} = event.data
-        await User.findByIdAndUpdate()
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id,userData)
     }
 )
 
-export const functions = [syncUserCreation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
