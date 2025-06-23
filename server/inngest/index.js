f5d5ecfe02db18a5d51757613c9983ab127e17a0
sandeep-kumar-21 <sandeepkumarnitrr@gmail.com>
2025-06-23 12:52:27 +0530

    confimation_email

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
-import sendEmail from "../config/nodemailer.js";
+import sendEmail from "../config/nMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
